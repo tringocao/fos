5f86845a153d58bf658ab2520cf6917d08464043
Minh-Thuc <lnmthuc@gmail.com>
2019-08-18 23:10:33 +0700

    Setup project structure

---
+++ fos-api/FOS/FOS.Repositories/Migrations/Configuration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOS.Repositories.Migrations
+{
+    public sealed class Configuration : DbMigrationsConfiguration<FosContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            CommandTimeout = int.MaxValue;
+        }
+        protected override void Seed(FosContext context)
+        {
+
+        }
+    }
+}
