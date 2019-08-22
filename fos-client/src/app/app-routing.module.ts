55fb3cd2be0698c7a0147532b1db47f06bd0aeaf
hau nguyen <conghau.nguyen95@gmail.com>
2019-08-22 16:27:46 +0700

    create event form

--- fos-client/src/app/app-routing.module.ts
+++ fos-client/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventFormComponent } from './event-form/event-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'event-form', component: EventFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ fos-client/src/app/event-form/event-form.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-form',
+  templateUrl: './event-form.component.html',
+  styleUrls: ['./event-form.component.less']
+})
+export class EventFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + ' ' + time;
+    console.log(dateTime);
+    // document.getElementById("datetimepicker1").value = "Johnny Bravo";
+  }
+
+}
