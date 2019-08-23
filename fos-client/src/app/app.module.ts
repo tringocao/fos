be75cb5142a3f7e9e74910da169d8f9de449989d
hau nguyen <conghau.nguyen95@gmail.com>
2019-08-23 17:50:03 +0700

    Update user picker

--- fos-client/src/app/app.module.ts
+++ fos-client/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventFormComponent } from './event-form/event-form.component';
+import { DlDateTimeDateModule, DlDateTimePickerModule } from 'angular-bootstrap-datetimepicker';
+import { FormsModule } from '@angular/forms';
+import { AngularDateTimePickerModule } from 'angular2-datetimepicker';
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,12 @@ import { EventFormComponent } from './event-form/event-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DlDateTimeDateModule,
+    DlDateTimePickerModule,
+    FormsModule,
+    AngularDateTimePickerModule,
+    AngularMultiSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- fos-client/src/app/event-form/event-form.component.ts
+++ fos-client/src/app/event-form/event-form.component.ts
@@ -12,33 +12,87 @@ export class EventFormComponent implements OnInit {
     { name: 'Kane', email: 'Tottenham Hospur' },
     { name: 'Hazard', email: 'Real Madrid' },
     { name: 'Griezmann', email: 'Barcelona' },
-];
-items: any[] = [
-  {id: 9, name :''},
-  { id: 0, name: 'one' },
-  { id: 1, name: 'two' },
-  { id: 2, name: 'three' },
-  { id: 3, name: 'four' },
-  { id: 4, name: 'five' },
-  { id: 5, name: 'six}' }
-];
-  constructor() { }
+  ];
+  items: any[] = [
+    { id: 0, name: 'one' },
+    { id: 1, name: 'two' },
+    { id: 2, name: 'three' },
+    { id: 3, name: 'four' },
+    { id: 4, name: 'five' },
+    { id: 5, name: 'six}' }
+  ];
+  selected: number = 0;
+
+  dateTimeToClose: Date = new Date();
+  dateTimeToReminder: Date = new Date();
+  settings = {
+    bigBanner: true,
+    timePicker: true,
+    format: 'dd-MM-yyyy hh:mm',
+    // defaultOpen: true
+  }
+  dropdownList = [];
+  selectedItems = [];
+  dropdownSettings = {};
+  constructor() {
+
+  }
 
   ngOnInit() {
-    // var today = new Date();
-    // var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    // var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    // var dateTime = date + ' ' + time;
-    // console.log(dateTime);
-    // // document.getElementById("datetimepicker1").value = "Johnny Bravo";
-    // document.getElementById("datetimepicker1").click();
-    // console.log("click date time picker");
+    this.dropdownList = [
+      { "id": 1, "itemName": "India" },
+      { "id": 2, "itemName": "Singapore" },
+      { "id": 3, "itemName": "Australia" },
+      { "id": 4, "itemName": "Canada" },
+      { "id": 5, "itemName": "South Korea" },
+      { "id": 6, "itemName": "Germany" },
+      { "id": 7, "itemName": "France" },
+      { "id": 8, "itemName": "Russia" },
+      { "id": 9, "itemName": "Italy" },
+      { "id": 10, "itemName": "Sweden" }
+    ];
+    this.selectedItems = [
+      { "id": 2, "itemName": "Singapore" },
+      { "id": 3, "itemName": "Australia" },
+      { "id": 4, "itemName": "Canada" },
+      { "id": 5, "itemName": "South Korea" }
+    ];
+    this.dropdownSettings = {
+      singleSelection: false,
+      text: "Select Countries",
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      enableSearchFilter: true,
+      classes: "myclass custom-class"
+    };
 
-    
   }
-  add(name: string): void {
-    console.log(name + "-----");
-    this.eventusers.push({ 'name':"new name", 'email': "new email"});
+  add(): void {
+    // console.log(this.nu + "-----");
+    this.eventusers.push({ 'name': "new name", 'email': "new email" });
   }
+  showSelectValue(id: string) {
+    //getted from event
+    console.log("option thay doi");
 
+    //getted from binding
+    // console.log(this.number)
+  }
+  showValue(): void {
+    console.log(this.dateTimeToClose);
+  }
+  onItemSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
+  }
+  OnItemDeSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
+  }
+  onDeSelectAll(items: any) {
+    console.log(items);
+  }
 }
