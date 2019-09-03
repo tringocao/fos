47e45f9a8bd84e4656e50c81d37b0be6eee5b686
tringocao <40257260+tringocao@users.noreply.github.com>
2019-09-03 09:08:36 +0700

    categories on client

--- fos-api/FOS/FOS.API/App_Start/UnityConfig.cs
+++ fos-api/FOS/FOS.API/App_Start/UnityConfig.cs
@@ -98,7 +98,10 @@ namespace FOS.API
                 new TransientLifetimeManager(),
                 new Interceptor<InterfaceInterceptor>(),
                 new InterceptionBehavior<LoggingInterceptor>());
-
+            container.RegisterType<Services.FoodServices.IFoodService, Services.FoodServices.FoodService>(
+                new TransientLifetimeManager(),
+                new Interceptor<InterfaceInterceptor>(),
+                new InterceptionBehavior<LoggingInterceptor>());
             //container.RegisterType<Services.ICrawlLinksService, Services.CrawlLinksService>();
             //container.RegisterType<Repositories.APIExternalServiceEntities, Repositories.APIExternalServiceEntities>(new PerResolveLifetimeManager());
             //container.RegisterType<Services.RequestMethods.IRequestMethod, Services.RequestMethods.GetMethod> ("GetMethod");
--- fos-client/src/app/models/categories.ts
+++ fos-client/src/app/models/categories.ts
@@ -2,10 +2,10 @@ class Category {
     name: string;
     id: string;
     code: string;
-  }
+}
 class CategoryGroup {
     name: string;
     id: string;
     code: string;
     categories: Category[];
-  }
+}
