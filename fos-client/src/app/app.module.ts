3baeef619c2c680e403cfe74ea191ae83ee7cee9
Truong Le <truong.le.quang@preciofishbone.se>
2019-09-11 15:18:16 +0700

    commit change before merge

--- fos-client/src/app/app.module.ts
+++ fos-client/src/app/app.module.ts
@@ -1,30 +1,30 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { TestComponent } from "./components/test/test.component";
-import { OrderService } from "./services/order/order.service";
-import { HttpClientModule } from "@angular/common/http";
-import { ListRestaurantComponent } from "./components/list-restaurant/list-restaurant.component";
-import { ServiceTabComponent } from "./components/service-tab/service-tab.component";
-import { RestaurantsPageComponent } from "./pages/restaurants-page/restaurants-page.component";
-import { MatCheckboxModule } from "@angular/material/checkbox";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TestComponent } from './components/test/test.component';
+import { OrderService } from './services/order/order.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ListRestaurantComponent } from './components/list-restaurant/list-restaurant.component';
+import { ServiceTabComponent } from './components/service-tab/service-tab.component';
+import { RestaurantsPageComponent } from './pages/restaurants-page/restaurants-page.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
-import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
-import { HTTP_INTERCEPTORS } from "@angular/common/http";
-import { TokenInterceptor } from "./auth/TokenInterceptor";
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from './auth/TokenInterceptor';
 
-import { CookieService } from "ngx-cookie-service";
-import { AuthService } from "./auth/auth.service";
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from './auth/auth.service';
 
-import { HeaderComponent } from "./components/navigation/header/header.component";
-import { SidenavListComponent } from "./components/navigation/sidenav-list/sidenav-list.component";
+import { HeaderComponent } from './components/navigation/header/header.component';
+import { SidenavListComponent } from './components/navigation/sidenav-list/sidenav-list.component';
 
-import { OrdersPageComponent } from "./pages/orders-page/orders-page.component";
-import { MealsPageComponent } from "./pages/meals-page/meals-page.component";
+import { OrdersPageComponent } from './pages/orders-page/orders-page.component';
+import { MealsPageComponent } from './pages/meals-page/meals-page.component';
 
 import {
   MatTableModule,
@@ -35,16 +35,16 @@ import {
   MatListModule,
   MatInputModule,
   MatTooltipModule
-} from "@angular/material";
+} from '@angular/material';
 import {
   DlDateTimeDateModule,
   DlDateTimePickerModule
-} from "angular-bootstrap-datetimepicker";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { AngularDateTimePickerModule } from "angular2-datetimepicker";
-import { AngularMultiSelectModule } from "angular2-multiselect-dropdown";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { SearchComponent } from "./components/search/search.component";
+} from 'angular-bootstrap-datetimepicker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularDateTimePickerModule } from 'angular2-datetimepicker';
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchComponent } from './components/search/search.component';
 
 import {
   MatButtonModule,
@@ -54,29 +54,30 @@ import {
   MatMenuModule,
   MatIconModule,
   MatProgressSpinnerModule
-} from "@angular/material";
+} from '@angular/material';
 
-import { MatSelectModule } from "@angular/material/select";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { SelectAutocompleteModule } from "mat-select-autocomplete";
-import { MenuComponent } from "./components/menu/menu.component";
-import { DialogComponent } from "./components/dialog/dialog.component";
+import { MatSelectModule } from '@angular/material/select';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { SelectAutocompleteModule } from 'mat-select-autocomplete';
+import { MenuComponent } from './components/menu/menu.component';
+import { DialogComponent } from './components/dialog/dialog.component';
 
-import { MatProgressBarModule } from "@angular/material/progress-bar";
-import { FoodCategoriesComponent } from "./components/dialog/food-categories/food-categories.component";
-import { LoadingComponent } from "./components/loading/loading.component";
-import { OrderTabComponent } from "./components/order-tab/order-tab.component";
-import { ListOrderComponent } from "./components/list-order/list-order.component";
-import { MatAutocompleteModule } from "@angular/material/autocomplete";
-import { OrderDetailComponent } from "./components/order-detail/order-detail.component";
-import { MenuEventComponent } from "./components/menu-event/menu-event.component";
-import { EventDialogComponent } from "./components/event-dialog/event-dialog.component";
-import { EventDialogViewComponent } from "./components/event-dialog-view/event-dialog-view.component";
-import { NgxStarsModule } from "ngx-stars";
-import { ShowActionComponent } from "./components/show-action/show-action.component";
-import { EventSummaryDialogComponent } from "./components/event-summary-dialog/event-summary-dialog.component";
-import { FoodComponent } from "./components/dialog/food/food.component";
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { FoodCategoriesComponent } from './components/dialog/food-categories/food-categories.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { OrderTabComponent } from './components/order-tab/order-tab.component';
+import { ListOrderComponent } from './components/list-order/list-order.component';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { OrderDetailComponent } from './components/order-detail/order-detail.component';
+import { MenuEventComponent } from './components/menu-event/menu-event.component';
+import { EventDialogComponent } from './components/event-dialog/event-dialog.component';
+import { EventDialogViewComponent } from './components/event-dialog-view/event-dialog-view.component';
+import { NgxStarsModule } from 'ngx-stars';
+import { ShowActionComponent } from './components/show-action/show-action.component';
+import { EventSummaryDialogComponent } from './components/event-summary-dialog/event-summary-dialog.component';
+import { FoodComponent } from './components/dialog/food/food.component';
 import { ListOrderedFoodsComponent } from './components/order-detail/list-ordered-foods/list-ordered-foods.component';
+import { ReminderDialogComponent } from './components/reminder-dialog/reminder-dialog.component';
 
 @NgModule({
   declarations: [
@@ -104,8 +105,7 @@ import { ListOrderedFoodsComponent } from './components/order-detail/list-ordere
     EventDialogViewComponent,
     EventSummaryDialogComponent,
     FoodComponent,
-    ListOrderedFoodsComponent
-    EventSummaryDialogComponent,
+    ListOrderedFoodsComponent,
     ReminderDialogComponent
   ],
   // declarations: [
