4329fcac4dba4d081607d3b35fba8f58e46b41ef
duy <duyhuynh>
2019-09-15 17:04:06 +0700

    fix event view, show order status

--- fos-client/src/app/components/event-dialog-view/event-dialog-view.component.ts
+++ fos-client/src/app/components/event-dialog-view/event-dialog-view.component.ts
@@ -15,6 +15,7 @@ import { EventUser } from 'src/app/models/eventuser';
 import { OrderService } from 'src/app/services/order/order.service';
 import { from } from 'rxjs';
 import { GraphUser } from 'src/app/models/graph-user';
+import { element } from 'protractor';
 @Component({
   selector: 'app-event-dialog-view',
   templateUrl: './event-dialog-view.component.html',
@@ -22,10 +23,12 @@ import { GraphUser } from 'src/app/models/graph-user';
 })
 export class EventDialogViewComponent implements OnInit {
   @ViewChild(MatTable, { static: true }) table: MatTable<any>;
-  eventusers: EventUser[] = [  ];
-  constructor(@Inject(MAT_DIALOG_DATA) public data: Event,
-  public dialogRef: MatDialogRef<EventDialogViewComponent>,
-  private orderService: OrderService) {}
+  eventusers: EventUser[] = [];
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: Event,
+    public dialogRef: MatDialogRef<EventDialogViewComponent>,
+    private orderService: OrderService
+  ) {}
   eventTitle = '';
   eventHost = '';
   eventRestaurant = '';
@@ -34,7 +37,7 @@ export class EventDialogViewComponent implements OnInit {
   dateToReminder = '';
   displayedColumns = ['avatar', 'Name', 'Email', 'Order Status'];
   EventTime = '';
-  EventStatus ='';
+  EventStatus = '';
   EventType = '';
   ngOnInit() {
     //get user not order
@@ -42,60 +45,57 @@ export class EventDialogViewComponent implements OnInit {
     console.log('EventId', this.data.EventId);
     var participants = JSON.parse(this.data.EventParticipantsJson);
     var userNotOrder = [];
-
-
-    // var userOrder: EventUser = {
-    //   Name: 'abc',
-    //   Email: 'mail',
-    //   Id: 'id1',
-    //   Img: '',
-    //   IsGroup: 0,
-    //   OrderStatus: 'not order'
-    // }
-    // self.eventusers.push(userOrder);
-
-    // let array = [1,2,3];
-    // for (let i = 0; i < array.length; i++) {
-    //   const userOrder: EventUser = {
-    //     Name: 'abc',
-    //     Email: 'mail',
-    //     Id: 'id1',
-    //     Img: '',
-    //     IsGroup: 0,
-    //     OrderStatus: 'not order'
-    //   }
-    //   self.eventusers.push(userOrder);
-    // }
-
-
-    this.orderService.GetUserNotOrdered(this.data.EventId).then(
-      result => {
+    let promise = this.orderService
+      .GetUserNotOrdered(this.data.EventId)
+      .then(result => {
         result.forEach(element => {
-          // console.log(element);
-          // const participants: GraphUser[] = JSON.parse(
-          //   this.data.EventParticipantsJson
-          // );
+          var participant = participants.filter(
+            item => item.id === element.UserId
+          );
 
-          // const participant = participants.filter(
-          //   item => item.id === element.UserId
-          // );
-          // console.log('participant: ', participant);
-
-          const userOrder: EventUser = {
-            Name: element.UserId,
-            Email:'',
-            Id: 'id1',
-            Img: '',
-            IsGroup: 0,
-            OrderStatus: 'not order'
-
+          if (participant) {
+            const userOrder: EventUser = {
+              Name: participant[0].displayName,
+              Email: participant[0].mail,
+              Id: participant[0].id,
+              Img: '',
+              IsGroup: 0,
+              OrderStatus: 'Not order'
+            };
+            this.eventusers.push(userOrder);
           }
-          this.eventusers.push(userOrder);
           this.table.renderRows();
         });
-      }
-    )
-    
+      });
+
+    promise.then(function() {
+      var p = participants;
+      var e = self.eventusers;
+      participants.forEach(element => {
+        var flag: Boolean = false;
+
+        self.eventusers.forEach(element2 => {
+          if (element.id === element2.Id) {
+            flag = true;
+          }
+        });
+
+        if (flag === false) {
+          console.log(element.displayName);
+          const userOrder: EventUser = {
+            Name: element.displayName,
+            Email: element.mail,
+            Id: element.id,
+            Img: '',
+            IsGroup: 0,
+            OrderStatus: 'Order'
+          };
+          self.eventusers.push(userOrder);
+          self.table.renderRows();
+        }
+      });
+    });
+
     // setTimeout(function(){
     //   // console.log(userNotOrder);
     //   participants.map(
@@ -121,32 +121,24 @@ export class EventDialogViewComponent implements OnInit {
 
     // },2000)
 
-
-
-
-    
     // console.log(self.eventusers);
     this.eventTitle = this.data.Name;
     this.eventHost = this.data.HostName;
     this.eventRestaurant = this.data.Restaurant;
     this.maximumBudget = this.data.MaximumBudget.toString();
-    this.dateTimeToClose = this.data.CloseTime
-      .toString()
+    this.dateTimeToClose = this.data.CloseTime.toString()
       .replace('T', ' ')
       .replace('+07:00', '');
-    this.dateToReminder = this.data.RemindTime
-      .toString()
+    this.dateToReminder = this.data.RemindTime.toString()
       .replace('T', ' ')
       .replace('+07:00', '');
 
-      this.EventTime = this.data.EventDate
-      .toString()
+    this.EventTime = this.data.EventDate.toString()
       .replace('T', ' ')
       .replace('+07:00', '');
-      this.EventStatus = this.data.Status;
-      this.EventType = this.data.EventType;
+    this.EventStatus = this.data.Status;
+    this.EventType = this.data.EventType;
 
-    
     // participants.map(value => {
     //   console.log('paricipant ', value);
     //   this.eventusers.push({
@@ -158,8 +150,6 @@ export class EventDialogViewComponent implements OnInit {
     //     OrderStatus: 'not order'
     //   });
     // })
-
-    
   }
   OnNoClick(): void {
     this.dialogRef.close();
