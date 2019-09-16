fef3a905cc16dd852a80a16836907f9af1258854
Dat Nguyen Tan <dattgk97@gmail.com>
2019-09-16 16:45:39 +0700

    group comment

--- fos-client/src/app/components/event-summary-dialog/event-summary-dialog.component.ts
+++ fos-client/src/app/components/event-summary-dialog/event-summary-dialog.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 import { MatTableDataSource } from '@angular/material';
 import { RestaurantService } from 'src/app/services/restaurant/restaurant.service';
 import { ActivatedRoute } from '@angular/router';
-
+ 
 import * as jsPDF from 'jspdf';
 // import * as printJs from 'printjs';
 import html2canvas from 'html2canvas';
@@ -18,7 +18,7 @@ import * as moment from 'moment';
 import 'moment/locale/vi';
 import { SummaryService } from 'src/app/services/summary/summary.service';
 import { Event } from "src/app/models/event";
-
+ 
 import { environment } from 'src/environments/environment';
 import { Report } from 'src/app/models/report';
 import { async } from 'q';
@@ -26,8 +26,8 @@ import { EventFormService } from 'src/app/services/event-form/event-form.service
 import { OrderService } from 'src/app/services/order/order.service';
 import { Food } from 'src/app/models/food';
 import { UserService } from 'src/app/services/user/user.service';
-
-
+ 
+ 
 @Component({
   selector: 'app-event-summary-dialog',
   templateUrl: './event-summary-dialog.component.html',
@@ -35,7 +35,7 @@ import { UserService } from 'src/app/services/user/user.service';
 })
 export class EventSummaryDialogComponent implements OnInit {
   @ViewChild('personGroupView', { static: false }) userGroupTab: ElementRef;
-
+ 
   constructor(
     private router: Router,
     private restaurantService: RestaurantService,
@@ -47,14 +47,14 @@ export class EventSummaryDialogComponent implements OnInit {
   ) {
     console.log(router.routerState);
   }
-
+ 
   eventDataAvailable:boolean;
   dishViewDataAvailable:boolean;
   personViewDataAvailable:boolean;
   printMode:boolean;
   dishGroupViewdataSource: any = new MatTableDataSource([]);
   personGroupViewdataSource: any = new MatTableDataSource([]);
-
+ 
   dishGroupViewDisplayedColumns: string[] = [
     'picture',
     'name',
@@ -70,14 +70,14 @@ export class EventSummaryDialogComponent implements OnInit {
     'pay-extra',
     'comment'
   ];
-
+ 
   restaurant: any;
-
+ 
   eventDetail: Event;
   foods: any[];
   orderByDish: any[] = [];
   orderByPerson: any[] = [];
-
+ 
   toStandardDate(date: Date) {
     return moment(date).format('DD/MM/YYYY HH:mm');
   }
@@ -85,7 +85,7 @@ export class EventSummaryDialogComponent implements OnInit {
   printToPdf() {
     // this.printMode = true;
     const printContent = document.getElementById("print");
-
+ 
     // printJs('print', 'html');
     console.log(printContent)
     const WindowPrt = window.open('', '', 'left=0,top=0,width=900,height=900,toolbar=0,scrollbars=0,status=0');
@@ -97,7 +97,7 @@ export class EventSummaryDialogComponent implements OnInit {
     WindowPrt.print();
     WindowPrt.close();
   }
-
+ 
   async sendEmail() {
     const page = document.getElementById('print');
     const options = {
@@ -112,13 +112,13 @@ export class EventSummaryDialogComponent implements OnInit {
       // let userGroupData = userTabSource.toDataURL("image/PNG")
       // Add image Canvas to PDF%
       // doc.addImage(pageData, 'PNG', 0, 0, window.innerWidth*0.25, window.innerHeight*0.25);
-
+ 
       this.summaryService.addReport(this.eventDetail.EventId, window.location.href, pageData)
       // doc.addImage(userGroupData, 'PNG', 20, 20, 200, 200);
       console.log('html2canvas');
     });
   }
-
+ 
   ngOnInit() {
     this.restaurant = { }
     this.eventDataAvailable = false;
@@ -126,7 +126,7 @@ export class EventSummaryDialogComponent implements OnInit {
     this.printMode = false;
     this.personViewDataAvailable = false;
     this.dishViewDataAvailable = false;
-
+ 
     this.route.params.subscribe(params => {
       var id = params['id'];
       this.eventFormService
@@ -138,7 +138,7 @@ export class EventSummaryDialogComponent implements OnInit {
           console.log(result[0])
           this.restaurant = result[0];
           this.restaurant.address = result[0].Address;
-
+ 
           this.restaurantService.getRestaurantDetail(Number(this.restaurant.DeliveryId))
           .then(result => {
             this.restaurant.Rating = Number(result.Rating);
@@ -160,13 +160,11 @@ export class EventSummaryDialogComponent implements OnInit {
               name: food.Value.Name,
               price: Number(food.Value.Price),
               picture: food.Value.Photo,
-              comments: [
-                {
-                  comment: food.Value.Comment,
-                  amount: 1
-                }
-              ],
-              totalComment: food.Value.Comment,
+              comments: [{
+                comment: food.Value.Comment,
+                amount: 1
+              }],
+              totalComment: '',
               amount: Number(food.Value.Amount),
               total: 0
             }
@@ -177,57 +175,75 @@ export class EventSummaryDialogComponent implements OnInit {
             }
             else {
               var selectedFood = this.foods.findIndex(f => f.foodId == food.IdFood);
-              console.log(selectedFood)
+              // console.log(selectedFood)
               this.foods[selectedFood].amount += _food.amount;
               this.foods[selectedFood].total += _food.total;
-              // this.foods[selectedFood].comments.forEach(_comment => {
-              //   // if (food.c) {
-
-              //   // }
-              // });
-              this.foods[selectedFood].totalComment += ' ' + _food.totalComment;
-              // if (comment == _food.comment) {
-              //   this.foods[selectedFood].comment = _food.tota
-              // }
-              // this.foods[selectedFood].comment += _food.tota
+              console.log(this.foods[selectedFood].comments)
+              console.log(food)
+              if (this.foods[selectedFood].comments.some(_comment => _comment.comment == food.Value.Comment)) {
+                var duplicatedFood = this.foods[selectedFood].comments.findIndex(c => c.comment == food.Value.Comment);
+                console.log(duplicatedFood)
+                this.foods[selectedFood].comments[duplicatedFood].amount++;
+              }
+              else {
+                this.foods[selectedFood].comments.push({
+                  comment: food.Value.Comment,
+                  amount: 1
+                })
+              }
+              this.foods[selectedFood].totalComment +=  _food.totalComment;
             }
           })
         })
-        this.dishGroupViewdataSource = this.foods;
         console.log(this.foods)
         this.dishViewDataAvailable = true;
-
+ 
         orders.forEach(order => {
           var orderItem = {
-            user: 'admin',
-            food: '1xChicken rice + 1x coca',
-            price: 40000,
-            payExtra: 5000,
-            comment: 'không hành'
+            user: '',
+            food: '',
+            price: 0,
+            payExtra: 0,
+            comments: [],
           };
           this.userService.getUserById(order.IdUser).then(user => {
             orderItem.user = user.DisplayName;
           }).then(() => {
             var foods = "";
-            var comment = "";
+            var comments = [];
             var total = 0;
             order.FoodDetail.forEach(food => {
               foods += food.Value.Amount + 'x ' + food.Value.Name + ', ';
-              comment += ' ' + food.Value.Comment; // try if same name
+              // comment += ' ' + food.Value.Comment;
+              if (comments.some(_comment => _comment.comment == food.Value.Comment)) {
+                var duplicatedComment = comments.findIndex(c => c.comment == food.Value.Comment);
+                comments[duplicatedComment].amount++;
+              }
+              else {
+                comments.push({
+                  comment: food.Value.Comment,
+                  amount: 1,
+                })
+              }
               total += Number(food.Value.Total);
             })
             orderItem.food = foods;
-            orderItem.payExtra = (Number(this.eventDetail.MaximumBudget) < total) ? (total - Number(this.eventDetail.MaximumBudget)) : 0;
-            orderItem.comment = comment;
-
+            orderItem.comments = comments;
+            orderItem.price = total;
+            if (this.eventDetail && this.eventDetail.MaximumBudget) {
+              orderItem.payExtra = (Number(this.eventDetail.MaximumBudget) < total) ? (total - Number(this.eventDetail.MaximumBudget)) : 0;
+            }
+            // orderItem.comment = comment;
+ 
             this.orderByPerson.push(orderItem)
             this.personViewDataAvailable = this.orderByPerson.length == orders.length;
           })
         })
+        this.dishGroupViewdataSource = this.foods;
+        this.personGroupViewdataSource = this.orderByPerson;
       })
     })
-
+ 
     // this.dishGroupViewdataSource = this.orderByDish;
-    this.personGroupViewdataSource = this.orderByPerson;
   }
 }
