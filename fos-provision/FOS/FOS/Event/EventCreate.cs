5e77626977b125a6644e526e13f249a46ae5ab76
hau nguyen <conghau.nguyen95@gmail.com>
2019-09-16 08:15:00 +0700

    upload Provision source

---
+++ fos-provision/FOS/FOS/Event/EventCreate.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOS.Event
+{
+    public static class EventCreate
+    {
+        public static void Create(ClientContext context)
+        {
+            EventSiteColumns.CreateColumn(context);
+            EventContentType.Create(context);
+            EventList.CreateList(context);
+            Console.WriteLine("Provision finished!");
+        }
+    }
+}
