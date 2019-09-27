d51c4eb9a5a07787d24988f64d4ed3da8c31ad7c
Dat Nguyen Tan <dattgk97@gmail.com>
2019-09-27 08:42:03 +0700

    models for report

---
+++ fos-api/FOS/FOS.Model/Dto/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOS.Model.Dto
+{
+    public class Comment
+    {
+        public string Value { get; set; }
+        public int Amount { get; set; }
+    }
+}
