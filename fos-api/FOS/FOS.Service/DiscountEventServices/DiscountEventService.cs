eca03d91552d11239e624c18ef3fd9ae00760843
tringocao <40257260+tringocao@users.noreply.github.com>
2019-09-30 20:13:01 +0700

    discount event

---
+++ fos-api/FOS/FOS.Service/DiscountEventServices/DiscountEventService.cs
@@ -0,0 +1,56 @@
+﻿using FOS.Repositories.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FOS.Repositories.Repositories;
+using System.Threading.Tasks;
+using FOS.Model.Domain;
+
+namespace FOS.Services.DiscountEventServices
+{
+    public class DiscountEventService: IDiscountEventService
+    {
+        IDiscountEventRepository _discountEventRepository;
+        IDiscountEventMapper _discountEventMapper;
+        public DiscountEventService(IDiscountEventRepository discountEventRepository, IDiscountEventMapper discountEventMapper)
+        {
+            _discountEventMapper = discountEventMapper;
+            _discountEventRepository = discountEventRepository;
+        }
+
+
+        public bool AddDiscountEvent(DiscountEvent discountEvent)
+        {
+            Repositories.DataModel.DiscountEvent temp = new Repositories.DataModel.DiscountEvent();
+            _discountEventMapper.MapToEfObject(temp, discountEvent);
+            return _discountEventRepository.AddDiscountEvent(temp);
+        }
+
+        public bool DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DiscountEvent> GetAllDiscountEvents()
+        {
+            return _discountEventRepository.GetAllDiscountEvents().Select(r => _discountEventMapper.MapToDomain(r));
+        }
+
+        public DiscountEvent GetByEventId(int eventId)
+        {
+            return _discountEventMapper.MapToDomain(_discountEventRepository.GetByEventId(eventId));
+        }
+
+        public DiscountEvent GetById(int id)
+        {
+            return _discountEventMapper.MapToDomain(_discountEventRepository.GetById(id));
+        }
+
+        public bool UpdateDiscountEvent(DiscountEvent discountEvent)
+        {
+            Repositories.DataModel.DiscountEvent temp = new Repositories.DataModel.DiscountEvent();
+            _discountEventMapper.MapToEfObject(temp, discountEvent);
+            return _discountEventRepository.UpdateDiscountEvent(temp);
+        }
+    }
+}
