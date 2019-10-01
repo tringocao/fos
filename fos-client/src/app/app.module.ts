c33bef74d042bf25e6a6d7392913088d4cfd40de
tringocao <40257260+tringocao@users.noreply.github.com>
2019-10-01 09:55:45 +0700

    merge dat

--- fos-client/src/app/app.module.ts
+++ fos-client/src/app/app.module.ts
@@ -101,10 +101,10 @@ import { WTimeComponent } from "./components/datetimepicker/time-dialog/w-time/w
 import { DialogCheckActionComponent } from "./components/order-detail/list-ordered-foods/dialog-check-action/dialog-check-action.component";
 import { UsersOrderedFoodDialogComponent } from "./components/users-ordered-food-dialog/users-ordered-food-dialog.component";
 import { OpenEventDialogComponent } from "./components/event-summary-dialog/open-event-dialog/open-event-dialog.component";
-import { EditMakeOrderComponent } from './components/event-summary-dialog/edit-make-order/edit-make-order.component';
-import { NotParticipantComponent } from './components/not-participant/not-participant.component';
-import { FeedbackComponent } from './components/feedback/feedback/feedback.component';
-import { StarRatingComponent } from './components/star-rating/star-rating.component';
+import { EditMakeOrderComponent } from "./components/event-summary-dialog/edit-make-order/edit-make-order.component";
+import { NotParticipantComponent } from "./components/not-participant/not-participant.component";
+import { FeedbackComponent } from "./components/feedback/feedback/feedback.component";
+import { StarRatingComponent } from "./components/star-rating/star-rating.component";
 
 @NgModule({
   declarations: [
@@ -152,8 +152,7 @@ import { StarRatingComponent } from './components/star-rating/star-rating.compon
     DialogCheckActionComponent,
     UsersOrderedFoodDialogComponent,
     OpenEventDialogComponent,
-    EditMakeOrderComponent
-    OpenEventDialogComponent,
+    EditMakeOrderComponent,
     NotParticipantComponent,
     FeedbackComponent,
     StarRatingComponent
