b9a41b4ccb5a4ec560220e1e8611197ac3eec21b
tringocao <40257260+tringocao@users.noreply.github.com>
2019-10-07 13:30:35 +0700

    change theme

--- fos-client/src/app/components/order-detail/list-ordered-foods/dialog-check-action/dialog-check-action.component.ts
+++ fos-client/src/app/components/order-detail/list-ordered-foods/dialog-check-action/dialog-check-action.component.ts
@@ -6,6 +6,8 @@ import {
   MAT_DIALOG_DATA
 } from "@angular/material/dialog";
 import { OverlayContainer } from "@angular/cdk/overlay";
+import { DataRoutingService } from 'src/app/data-routing.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: "app-dialog-check-action",
   templateUrl: "./dialog-check-action.component.html",
@@ -16,9 +18,19 @@ export class DialogCheckActionComponent implements OnInit {
     public dialogRef: MatDialogRef<DialogCheckActionComponent>,
     @Inject(MAT_DIALOG_DATA) public data: FoodDetailJson,
     overlayContainer: OverlayContainer
-  ) {
-    overlayContainer.getContainerElement().classList.add("app-theme1-theme");
-  }
+    ,
+    private dataRouting: DataRoutingService
+ ) {
+      this.getNavTitleSubscription = this.dataRouting.getNavTitle()
+   .subscribe((appTheme: string) => this.appTheme = appTheme);
+   overlayContainer.getContainerElement().classList.add("app-"+this.appTheme+"-theme");
+ }
+ ngOnDestroy() {
+   // You have to `unsubscribe()` from subscription on destroy to avoid some kind of errors
+   this.getNavTitleSubscription.unsubscribe();
+ }
+ private getNavTitleSubscription: Subscription;
+ appTheme: string;
   ngOnInit() {}
   onNoClick(): void {
     this.dialogRef.close();
---
+++ fos-client/src/app/data-routing.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+
+@Injectable()
+export class DataRoutingService {
+  private navTitle$: BehaviorSubject<string> = new BehaviorSubject<string>(
+    "Default nav title"
+  );
+
+  public setNavTitle(newNavTitle: string): void {
+    // Sets new value, every entity, which is subscribed to changes (`getNavTitle().subscribe(...)`) will get new value every time it changes
+    this.navTitle$.next(newNavTitle);
+  }
+
+  public getNavTitle(): Observable<string> {
+    // Allow to `subscribe` on changes and get the value every time it changes
+    return this.navTitle$.asObservable();
+  }
+}
